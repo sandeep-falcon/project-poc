360adbcafca35805b75699b9053a398f7c260251
sandeepfalcon <sandeep@falconfs.com>
2024-09-03 17:16:35 +0530

    ADD: rxjs collection cards

--- src/app/components/rxjs-collection/rxjs-collection.component.ts
+++ src/app/components/rxjs-collection/rxjs-collection.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MatRippleModule } from '@angular/material/core';
+import { RouterLink } from '@angular/router';
+import { RxjsCollectionInterface } from '../../interface/rxjs-collection-interface';
 
 @Component({
   selector: 'app-rxjs-collection',
   standalone: true,
-  imports: [],
+  imports: [MatRippleModule, RouterLink],
   templateUrl: './rxjs-collection.component.html',
-  styleUrl: './rxjs-collection.component.scss'
+  styleUrl: './rxjs-collection.component.scss',
 })
 export class RxjsCollectionComponent {
-
+  rxJsCollection: RxjsCollectionInterface[] = [
+    // {
+    //   label: 'Observables',
+    //   route: '/observables',
+    // },
+    {
+      label: 'of and from',
+      route: '/ofandfrom',
+    },
+    {
+      label: 'CombineLatest and forkJoin',
+      route: '/clfj',
+    },
+  ];
 }
---
+++ src/app/interface/rxjs-collection-interface.ts
@@ -0,0 +1,4 @@
+export interface RxjsCollectionInterface {
+  label: string;
+  route: string;
+}
