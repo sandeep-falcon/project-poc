107d067b2c7e03ec111ae6a3aff9d15098f987e1
sandeepfalcon <sandeep@falconfs.com>
2024-09-09 16:20:29 +0530

    ADD: signal implement auto sidebar open/close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,18 +13,16 @@ import { Location } from '@angular/common';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  screenWidth!: number;
+  screenWidth = signal(window.innerWidth)
 
   constructor(private _location: Location) {
     this.calculateWidthOnScreenResize();
   }
- // Function to calculate the screen width on page load and on screen size change
+  // Function to calculate the screen width on page load and on screen size change
   calculateWidthOnScreenResize() {
     // set screenWidth on page load
-    this.screenWidth = window.innerWidth;
     window.onresize = () => {
-      // set screenWidth on screen size change
-      this.screenWidth = window.innerWidth;
+      this.screenWidth.set(window.innerWidth);
     };
   }
 
