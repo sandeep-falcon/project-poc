c29e81f387973b3f9510cd797b693f5db3f8cfa4
sandeep-falcon <sandeep@falconfs.com>
2025-12-15 16:33:55 +0530

    custom theme preset

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,19 +6,69 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { definePreset } from '@primeng/themes';
+
+const Noir = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{zinc.50}',
+            100: '{zinc.100}',
+            200: '{zinc.200}',
+            300: '{zinc.300}',
+            400: '{zinc.400}',
+            500: '{zinc.500}',
+            600: '{zinc.600}',
+            700: '{zinc.700}',
+            800: '{zinc.800}',
+            900: '{zinc.900}',
+            950: '{zinc.950}'
+        },
+        colorScheme: {
+            light: {
+                primary: {
+                    color: '{zinc.950}',
+                    contrastColor: '#ffffff',
+                    hoverColor: '{zinc.900}',
+                    activeColor: '{zinc.800}'
+                },
+                highlight: {
+                    background: '{zinc.950}',
+                    focusBackground: '{zinc.700}',
+                    color: '#ffffff',
+                    focusColor: '#ffffff'
+                }
+            },
+            dark: {
+                primary: {
+                    color: '{zinc.50}',
+                    contrastColor: '{zinc.950}',
+                    hoverColor: '{zinc.100}',
+                    activeColor: '{zinc.200}'
+                },
+                highlight: {
+                    background: '{zinc.50}',
+                    focusBackground: '{zinc.300}',
+                    color: '{zinc.950}',
+                    focusColor: '{zinc.950}'
+                }
+            }
+        }
+    }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideZonelessChangeDetection(),
     provideHttpClient(),
-    provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Aura
+            preset: Noir,
+            options: {
+                darkModeSelector: false || 'none'
+            }
         },
         ripple: true
     })
